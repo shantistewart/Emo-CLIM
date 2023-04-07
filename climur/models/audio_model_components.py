0ed08ffb3842d9a43b1629880c61b3ce304e4fe9
Shanti Stewart <shantiystewart@gmail.com>
2023-04-07 13:22:54 -0700

    Added Short-Chunk CNN ResNet audio backbone.

--- climur/models/audio_model_components.py
+++ climur/models/audio_model_components.py
@@ -11,6 +11,78 @@ import librosa
 
 """Full music tagging models. Following code is from https://github.com/minzwon/sota-music-tagging-models/blob/master/training/model.py."""
 
+class ShortChunkCNN_Res(nn.Module):
+    '''
+    Short-chunk CNN architecture with residual connections.
+    '''
+    def __init__(self,
+                n_channels=128,
+                sample_rate=16000,
+                n_fft=512,
+                f_min=0.0,
+                f_max=8000.0,
+                n_mels=128,
+                n_class=50):
+        super(ShortChunkCNN_Res, self).__init__()
+
+        # Spectrogram
+        self.spec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
+                                                         n_fft=n_fft,
+                                                         f_min=f_min,
+                                                         f_max=f_max,
+                                                         n_mels=n_mels)
+        self.to_db = torchaudio.transforms.AmplitudeToDB()
+        self.spec_bn = nn.BatchNorm2d(1)
+
+        # CNN
+        self.layer1 = Res_2d(1, n_channels, stride=2)
+        self.layer2 = Res_2d(n_channels, n_channels, stride=2)
+        self.layer3 = Res_2d(n_channels, n_channels*2, stride=2)
+        self.layer4 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer5 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer6 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer7 = Res_2d(n_channels*2, n_channels*4, stride=2)
+
+        # Dense
+        self.dense1 = nn.Linear(n_channels*4, n_channels*4)
+        self.bn = nn.BatchNorm1d(n_channels*4)
+        self.dense2 = nn.Linear(n_channels*4, n_class)
+        self.dropout = nn.Dropout(0.5)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # Spectrogram
+        x = self.spec(x)
+        x = self.to_db(x)
+        x = x.unsqueeze(1)
+        x = self.spec_bn(x)
+
+        # CNN
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = x.squeeze(2)
+
+        # Global Max Pooling
+        if x.size(-1) != 1:
+            x = nn.MaxPool1d(x.size(-1))(x)
+        x = x.squeeze(2)
+
+        # Dense
+        x = self.dense1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.dense2(x)
+        x = nn.Sigmoid()(x)
+
+        return x
+
+
 class HarmonicCNN(nn.Module):
     '''
     Won et al. 2020
@@ -87,6 +159,39 @@ class HarmonicCNN(nn.Module):
 
 """Music tagging model components. Following code is from https://github.com/minzwon/sota-music-tagging-models/blob/master/training/modules.py."""
 
+# ShortChunkCNN_Res components:
+
+class Res_2d(nn.Module):
+    def __init__(self, input_channels, output_channels, shape=3, stride=2):
+        super(Res_2d, self).__init__()
+        # convolution
+        self.conv_1 = nn.Conv2d(input_channels, output_channels, shape, stride=stride, padding=shape//2)
+        self.bn_1 = nn.BatchNorm2d(output_channels)
+        self.conv_2 = nn.Conv2d(output_channels, output_channels, shape, padding=shape//2)
+        self.bn_2 = nn.BatchNorm2d(output_channels)
+
+        # residual
+        self.diff = False
+        if (stride != 1) or (input_channels != output_channels):
+            self.conv_3 = nn.Conv2d(input_channels, output_channels, shape, stride=stride, padding=shape//2)
+            self.bn_3 = nn.BatchNorm2d(output_channels)
+            self.diff = True
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # convolution
+        out = self.bn_2(self.conv_2(self.relu(self.bn_1(self.conv_1(x)))))
+
+        # residual
+        if self.diff:
+            x = self.bn_3(self.conv_3(x))
+        out = x + out
+        out = self.relu(out)
+        return out
+
+
+# HarmonicCNN components:
+
 # Modules for harmonic filters
 def hz_to_midi(hz):
     return 12 * (torch.log2(hz) - np.log2(440.0)) + 69
@@ -120,7 +225,6 @@ def initialize_filterbank(sample_rate, n_harmonic, semitone_scale):
         harmonic_hz = np.concatenate((harmonic_hz, hz * (i+1)))
 
     return harmonic_hz, level
-
 
 class HarmonicSTFT(nn.Module):
     def __init__(self,
@@ -202,7 +306,6 @@ class HarmonicSTFT(nn.Module):
         harmonic_spec = self.amplitude_to_db(harmonic_spec)
         return harmonic_spec
 
-
 class Conv_2d(nn.Module):
     def __init__(self, input_channels, output_channels, shape=3, stride=1, pooling=2):
         super(Conv_2d, self).__init__()
@@ -213,7 +316,6 @@ class Conv_2d(nn.Module):
     def forward(self, x):
         out = self.mp(self.relu(self.bn(self.conv(x))))
         return out
-
 
 class Res_2d_mp(nn.Module):
     def __init__(self, input_channels, output_channels, pooling=2):
---
+++ tests/shortchunk_tests.py
@@ -0,0 +1,116 @@
+"""Script for testing Short-Chunk CNN ResNet model."""
+
+
+import os
+import torch
+import torchinfo
+import warnings
+from climur.models.audio_backbones import ShortChunkCNNEmbeddings
+from climur.models.audio_model_components import ShortChunkCNN_Res
+
+
+# constants:
+AUDIO_LENGTH = 59049     # ~3.69 seconds
+N_CLASSES = 50
+
+# script options:
+device = torch.device("cuda:1") if torch.cuda.is_available() else torch.device("cpu")
+batch_size = 16
+# model options:
+pretrained_full_model_path = "/proj/systewar/pretrained_models/music_tagging/msd/short_chunk_resnet/best_model.pth"
+last_layer_embed = "layer7"
+shrink_freq = True
+shrink_time = True
+pool_type = "max"
+# for model summaries:
+model_summaries = True
+if model_summaries:
+    summaries_dir = "tests/model_summaries"
+    model_summary_info = ["input_size", "output_size", "num_params"]
+
+
+if __name__ == "__main__":
+    print("\n\n")
+    # suppress warnings:
+    warnings.filterwarnings("ignore")
+
+
+    # test full Short-Chunk CNN ResNet model:
+    print("Testing full Short-Chunk CNN ResNet model:")
+
+    # load pretrained full Harmonic CNN model:
+    full_model = ShortChunkCNN_Res()
+    full_model.load_state_dict(torch.load(pretrained_full_model_path, map_location=device))
+    full_model.to(device)
+
+    # test forward pass:
+    print("Testing forward pass...")
+    x = torch.rand((batch_size, AUDIO_LENGTH))
+    x = x.to(device)
+    output = full_model(x)
+    assert tuple(output.size()) == (batch_size, N_CLASSES), "Error with shape of forward pass output."
+
+    # create model summary, if selected:
+    if model_summaries:
+        print("Creating model summary...")
+        os.makedirs(summaries_dir, exist_ok=True)
+        full_model_summary = str(torchinfo.summary(
+            full_model,
+            input_size=(batch_size, AUDIO_LENGTH),
+            col_names=model_summary_info,
+            depth=1,
+            verbose=0
+        ))
+        full_model_summary_file = os.path.join(summaries_dir, "shortchunk_full_model_summary.txt")
+        with open(full_model_summary_file, "w") as text_file:
+            text_file.write(full_model_summary)
+    
+
+    # test ShortChunkCNNEmbeddings model:
+    print("\n\nTesting ShortChunkCNNEmbeddings model:")
+
+    # create ShortChunkCNNEmbeddings model:
+    x = torch.rand((batch_size, AUDIO_LENGTH))
+    x = x.to(device)
+    embed_model = ShortChunkCNNEmbeddings(
+        full_model,
+        sample_input=x,
+        last_layer=last_layer_embed,
+        shrink_freq=shrink_freq,
+        shrink_time=shrink_time,
+        pool_type=pool_type
+    )
+    embed_model.to(device)
+
+    print("\nTesting forward pass...")
+    # test forward pass:
+    print("Input size: {}".format(tuple(x.size())))
+    output = embed_model(x)
+    print("Output size: {}".format(tuple(output.size())))
+
+    # tests:
+    if shrink_freq == True and shrink_time == True:
+        assert len(tuple(output.size())) == 2, "Error with shape of forward pass output."
+    elif (shrink_freq == True and shrink_time == False) or (shrink_freq == False and shrink_time == True):
+        assert len(tuple(output.size())) == 3, "Error with shape of forward pass output."
+    else:
+        assert len(tuple(output.size())) == 4, "Error with shape of forward pass output."
+    assert output.size(dim=0) == batch_size, "Error with shape of forward pass output."
+
+    # create model summary, if selected:
+    if model_summaries:
+        print("\nCreating model summary...")
+        os.makedirs(summaries_dir, exist_ok=True)
+        embed_model_summary = str(torchinfo.summary(
+            embed_model,
+            input_size=(batch_size, AUDIO_LENGTH),
+            col_names=model_summary_info,
+            depth=2,
+            verbose=0
+        ))
+        embed_model_summary_file = os.path.join(summaries_dir, "shortchunk_embed_model_summary.txt")
+        with open(embed_model_summary_file, "w") as text_file:
+            text_file.write(embed_model_summary)
+    
+    print("\n")
+
