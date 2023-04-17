0b88967e22fa90ddec35ff4ee9d73223de6354b8
klean2050 <k.avramidis@windowslive.com>
2023-04-17 14:57:27 -0700

    changes to run by @klean2050

--- climur/scripts/training.py
+++ climur/scripts/training.py
@@ -5,6 +5,7 @@ import argparse
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 import torch
 from torch.utils.data import DataLoader
 import clip
@@ -14,7 +15,12 @@ from climur.dataloaders.audioset import AudioSetMood
 from climur.dataloaders.multimodal import Multimodal
 from climur.models.image_backbones import CLIPModel
 from climur.models.audio_model_components import ShortChunkCNN_Res, HarmonicCNN
-from climur.models.audio_backbones import ShortChunkCNNEmbeddings, HarmonicCNNEmbeddings, SHORTCHUNK_INPUT_LENGTH, HARMONIC_CNN_INPUT_LENGTH
+from climur.models.audio_backbones import (
+    ShortChunkCNNEmbeddings,
+    HarmonicCNNEmbeddings,
+    SHORTCHUNK_INPUT_LENGTH,
+    HARMONIC_CNN_INPUT_LENGTH,
+)
 from climur.trainers.image2music import Image2Music
 from climur.utils.misc import load_configs
 
@@ -30,12 +36,11 @@ if __name__ == "__main__":
 
     # parse command-line arguments:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_file', nargs='?', const=CONFIG_FILE, default=CONFIG_FILE)
+    parser.add_argument("--config_file", nargs="?", const=CONFIG_FILE, default=CONFIG_FILE)
     args = parser.parse_args()
 
     # set PyTorch warnings:
     # torch.set_warn_always(False)
-
 
     # -------
     # CONFIGS
@@ -58,14 +63,13 @@ if __name__ == "__main__":
     if dataset_configs["random_seed"]:
         pl.seed_everything(dataset_configs["random_seed"], workers=True)
 
-
     # ---------------
     # BACKBONE MODELS
     # ---------------
 
     if verbose:
         print("\nSetting up backbone models...")
-    
+
     # load CLIP model:
     orig_clip_model, image_preprocess_transform = clip.load("ViT-B/32", device=device)
     # create CLIP wrapper model:
@@ -78,36 +82,43 @@ if __name__ == "__main__":
         audio_clip_length = SHORTCHUNK_INPUT_LENGTH
         # load pretrained full Short-Chunk CNN ResNet model:
         full_audio_backbone = ShortChunkCNN_Res()
-        full_audio_backbone.load_state_dict(torch.load(audio_backbone_configs["pretrained_model_path"], map_location=torch.device("cpu")))
+        full_audio_backbone.load_state_dict(
+            torch.load(
+                audio_backbone_configs["pretrained_model_path"], map_location=torch.device("cpu")
+            )
+        )
         # create wrapper model:
         sample_audio_input = torch.rand((2, audio_clip_length))
         audio_backbone = ShortChunkCNNEmbeddings(
             full_audio_backbone,
             sample_input=sample_audio_input,
             last_layer=audio_backbone_configs["last_layer_embed"],
-            pool_type=audio_backbone_configs["pool_type"]
+            pool_type=audio_backbone_configs["pool_type"],
         )
-    
+
     elif audio_backbone_configs["model_name"] == "HarmonicCNN":
         # set audio input length:
         audio_clip_length = HARMONIC_CNN_INPUT_LENGTH
         # load pretrained full Harmonic CNN model:
         full_audio_backbone = HarmonicCNN()
-        full_audio_backbone.load_state_dict(torch.load(audio_backbone_configs["pretrained_model_path"], map_location=torch.device("cpu")))
+        full_audio_backbone.load_state_dict(
+            torch.load(
+                audio_backbone_configs["pretrained_model_path"], map_location=torch.device("cpu")
+            )
+        )
         # create wrapper model:
         sample_audio_input = torch.rand((2, audio_clip_length))
         audio_backbone = HarmonicCNNEmbeddings(
             full_audio_backbone,
             sample_input=sample_audio_input,
             last_layer=audio_backbone_configs["last_layer_embed"],
-            pool_type=audio_backbone_configs["pool_type"]
+            pool_type=audio_backbone_configs["pool_type"],
         )
-    
+
     else:
         raise ValueError("{} model not supported".format(audio_backbone_configs["model_name"]))
 
     audio_backbone.to(device)
-
 
     # ------------
     # DATA LOADERS
@@ -115,17 +126,17 @@ if __name__ == "__main__":
 
     if verbose:
         print("\nSetting up datasets and data loaders...")
-    
+
     # create image datasets:
     image_train_dataset = IMACImages(
         root=dataset_configs["image_dataset_dir"],
         metadata_file_name="metadata_train.csv",
-        preprocess=image_preprocess_transform
+        preprocess=image_preprocess_transform,
     )
     image_val_dataset = IMACImages(
         root=dataset_configs["image_dataset_dir"],
         metadata_file_name="metadata_val.csv",
-        preprocess=image_preprocess_transform
+        preprocess=image_preprocess_transform,
     )
 
     # create audio datasets:
@@ -133,48 +144,61 @@ if __name__ == "__main__":
         root=dataset_configs["audio_dataset_dir"],
         metadata_file_name="metadata_unbalanced_train.csv",
         clip_length_samples=audio_clip_length,
-        sample_rate=dataset_configs["sample_rate"]
+        sample_rate=dataset_configs["sample_rate"],
     )
     audio_val_dataset = AudioSetMood(
         root=dataset_configs["audio_dataset_dir"],
-        metadata_file_name="metadata_balanced_train.csv",     # TODO: Reconsider AudioSet split.
+        metadata_file_name="metadata_balanced_train.csv",  # TODO: Reconsider AudioSet split.
         clip_length_samples=audio_clip_length,
-        sample_rate=dataset_configs["sample_rate"]
+        sample_rate=dataset_configs["sample_rate"],
     )
 
     # create multimodal datasets:
     multimodal_train_dataset = Multimodal(
         image_dataset=image_train_dataset,
         audio_dataset=audio_train_dataset,
-        length=dataset_configs["train_n_batches"] * training_configs["batch_size"]
+        length=dataset_configs["train_n_batches"] * training_configs["batch_size"],
     )
     multimodal_val_dataset = Multimodal(
         image_dataset=image_val_dataset,
         audio_dataset=audio_val_dataset,
-        length=dataset_configs["val_n_batches"] * training_configs["batch_size"]
+        length=dataset_configs["val_n_batches"] * training_configs["batch_size"],
     )
     if verbose:
-        print("For training: using {} batches per epoch, sampling from {} images and {} audio clips.".format(dataset_configs["train_n_batches"], multimodal_train_dataset.image_dataset_len, multimodal_train_dataset.audio_dataset_len))
-        print("For validation: using {} batches, sampling from {} images and {} audio clips.".format(dataset_configs["val_n_batches"], multimodal_val_dataset.image_dataset_len, multimodal_val_dataset.audio_dataset_len))
-    
+        print(
+            "For training: using {} batches per epoch, sampling from {} images and {} audio clips.".format(
+                dataset_configs["train_n_batches"],
+                multimodal_train_dataset.image_dataset_len,
+                multimodal_train_dataset.audio_dataset_len,
+            )
+        )
+        print(
+            "For validation: using {} batches, sampling from {} images and {} audio clips.".format(
+                dataset_configs["val_n_batches"],
+                multimodal_val_dataset.image_dataset_len,
+                multimodal_val_dataset.audio_dataset_len,
+            )
+        )
+
     # create dataloaders:
     train_loader = DataLoader(
         multimodal_train_dataset,
         batch_size=training_configs["batch_size"],
         shuffle=True,
         num_workers=training_configs["n_workers"],
-        drop_last=True
+        drop_last=True,
     )
-    assert len(train_loader) == dataset_configs["train_n_batches"], "Length of train_loader is incorrect."
+    assert (
+        len(train_loader) == dataset_configs["train_n_batches"]
+    ), "Length of train_loader is incorrect."
     val_loader = DataLoader(
         multimodal_val_dataset,
         batch_size=training_configs["batch_size"],
         shuffle=False,
         num_workers=training_configs["n_workers"],
-        drop_last=True
+        drop_last=True,
     )
     assert len(val_loader) == dataset_configs["val_n_batches"], "Length of val_loader is incorrect."
-
 
     # -------------------
     # FULL MODEL & LOGGER
@@ -182,52 +206,48 @@ if __name__ == "__main__":
 
     if verbose:
         print("\nSetting up full model and logger...")
-    
+
     full_model = Image2Music(
         image_backbone=image_backbone,
         audio_backbone=audio_backbone,
         joint_embed_dim=full_model_configs["joint_embed_dim"],
         image_embed_dim=image_backbone_configs["embed_dim"],
         audio_embed_dim=audio_backbone_configs["embed_dim"],
-        hparams=training_configs,     # TODO: Maybe change to include all configs (need to modify Image2Music class).
+        hparams=training_configs,  # TODO: Maybe change to include all configs (need to modify Image2Music class).
         normalize_image_embeds=full_model_configs["normalize_image_embeds"],
         normalize_audio_embeds=full_model_configs["normalize_audio_embeds"],
         freeze_image_backbone=full_model_configs["freeze_image_backbone"],
         freeze_audio_backbone=full_model_configs["freeze_audio_backbone"],
-        device=device
+        device=device,
     )
 
     # create logger (logs are saved to /save_dir/name/version/):
     logger = TensorBoardLogger(
         save_dir=logging_configs["log_dir"],
         name=logging_configs["experiment_name"],
-        version=logging_configs["experiment_version"]
+        version=logging_configs["experiment_version"],
     )
-
 
     # --------
     # TRAINING
     # --------
 
+    model_ckpt_callback = ModelCheckpoint(monitor="Valid/total_loss", mode="min", save_top_k=1)
+
     # create trainer:
     trainer = Trainer(
         logger=logger,
+        callbacks=[model_ckpt_callback],
         max_epochs=training_configs["max_epochs"],
         val_check_interval=training_configs["val_check_interval"],
         log_every_n_steps=logging_configs["log_every_n_steps"],
         # sync_batchnorm=True,     # TODO: Check if this is only required for multi-GPU training.
         accelerator="gpu",
-        devices = [training_configs["gpu"]]
+        devices=[training_configs["gpu"]]
         # deterministic="warn",     # set when running training sessions for reproducibility
     )
 
     # train model:
-    trainer.fit(
-        full_model,
-        train_dataloaders=train_loader,
-        val_dataloaders=val_loader
-    )
-
+    trainer.fit(full_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     print("\n\n")
-
