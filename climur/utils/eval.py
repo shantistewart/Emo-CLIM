0a8c5be258260c315aa6c4eace4a04a1c2514501
Shanti Stewart <shantiystewart@gmail.com>
2023-04-19 23:38:50 -0700

    Added code to extract image/audio embeddings.

---
+++ climur/utils/eval.py
@@ -0,0 +1,146 @@
+"""Utility functions for evaluation."""
+
+
+import torch
+from torch.utils.data import Dataset
+import tqdm
+from typing import Union, Dict, List, Tuple, Any
+
+
+def get_image_embeddings(model: Any, image_dataset: Dataset, image_dataset_tags: List, device: Any) -> Union[ Tuple[List, List, List], Tuple[List, List] ]:
+    """Extracts image embeddings and their emotion labels.
+
+    Args:
+        model (LightningModule): PyTorch Lightning LightningModule.
+        image_dataset (PyTorch Dataset): Image dataset.
+        image_dataset_tags (list): List of image dataset emotion tags to use.
+        device (PyTorch device): PyTorch device.
+    
+    Returns:
+        if model.multi_task == True:
+            image_intra_embeds (list): Image embeddings in intra-modal image embedding space.
+            image_cross_embeds (list): Image embeddings in (cross-modal) joint embedding space.
+            image_tags (list): Image emotion tags.
+        else:
+            image_embeds (list): Image embeddings (in joint embedding space).
+            image_tags (list): Image emotion tags.
+    """
+
+    # set model to eval mode if not already:
+    model.eval()
+
+    # extract image embeddings and emotion tags:     # TODO: Change to using dataloader instead of dataset class.
+    if model.multi_task:
+        image_intra_embeds = []
+        image_cross_embeds = []
+    else:
+        image_embeds = []
+    image_tags = []
+
+    for idx in tqdm.tqdm(range(len(image_dataset)), total=len(image_dataset), desc="Extracting image embeddings and labels"):
+        # get image and emotion tag:
+        image, tag = image_dataset[idx]
+        # insert batch dimension:
+        image = image.unsqueeze(dim=0)
+        image = image.to(device)
+        assert len(tuple(image.size())) == 4 and image.size(dim=1) == 3, "Error with image shape."
+
+        # compute image embedding if emotion tag class was used during training:
+        if tag in image_dataset_tags:
+            if model.multi_task:
+                with torch.no_grad():
+                    intra_embed, cross_embed = model.compute_image_embeds(image)
+                # remove batch dimension:
+                intra_embed = intra_embed.squeeze(dim=0)
+                cross_embed = cross_embed.squeeze(dim=0)
+
+                assert tuple(intra_embed.size()) == (model.output_embed_dim, ), "Error with shape of intra_embed."
+                assert tuple(cross_embed.size()) == (model.output_embed_dim, ), "Error with shape of cross_embed."
+                image_intra_embeds.append(intra_embed)
+                image_cross_embeds.append(cross_embed)
+            
+            else:
+                with torch.no_grad():
+                    embed = model.compute_image_embeds(image)
+                # remove batch dimension:
+                embed = embed.squeeze(dim=0)
+
+                assert tuple(embed.size()) == (model.output_embed_dim, ), "Error with shape of embed."
+                image_embeds.append(embed)
+            
+            image_tags.append(tag)
+    
+    if model.multi_task:
+        return image_intra_embeds, image_cross_embeds, image_tags
+    else:
+        return image_embeds, image_tags
+
+
+def get_audio_embeddings(model: Any, audio_dataset: Dataset, audio_dataset_tags: List, device: Any) -> Union[ Tuple[List, List, List], Tuple[List, List] ]:
+    """Extracts audio embeddings and their emotion labels.
+
+    Args:
+        model (LightningModule): PyTorch Lightning LightningModule.
+        audio_dataset (PyTorch Dataset): Audio dataset.
+        audio_dataset_tags (list): List of audio dataset emotion tags to use.
+        device (PyTorch device): PyTorch device.
+    
+    Returns:
+        if model.multi_task == True:
+            audio_intra_embeds (list): Audio embeddings in intra-modal audio embedding space.
+            audio_cross_embeds (list): Audio embeddings in (cross-modal) joint embedding space.
+            audio_tags (list): Audio emotion tags.
+        else:
+            audio_embeds (list): Audio embeddings (in joint embedding space).
+            audio_tags (list): Audio emotion tags.
+    """
+
+    # set model to eval mode if not already:
+    model.eval()
+    
+    # extract audio embeddings and emotion tags:     # TODO: Change to using dataloader instead of dataset class.
+    if model.multi_task:
+        audio_intra_embeds = []
+        audio_cross_embeds = []
+    else:
+        audio_embeds = []
+    audio_tags = []
+
+    for idx in tqdm.tqdm(range(len(audio_dataset)), total=len(audio_dataset), desc="Extracting audio embeddings and labels"):
+        # get audio and emotion tag:
+        audio, tag = audio_dataset[idx]
+        # insert batch dimension:
+        audio = audio.unsqueeze(dim=0)
+        audio = audio.to(device)
+        assert len(tuple(audio.size())) == 2, "Error with audio shape."
+
+        # compute audio embedding if emotion tag class was used during training:
+        if tag in audio_dataset_tags:
+            if model.multi_task:
+                with torch.no_grad():
+                    intra_embed, cross_embed = model.compute_audio_embeds(audio)
+                # remove batch dimension:
+                intra_embed = intra_embed.squeeze(dim=0)
+                cross_embed = cross_embed.squeeze(dim=0)
+
+                assert tuple(intra_embed.size()) == (model.output_embed_dim, ), "Error with shape of intra_embed."
+                assert tuple(cross_embed.size()) == (model.output_embed_dim, ), "Error with shape of cross_embed."
+                audio_intra_embeds.append(intra_embed)
+                audio_cross_embeds.append(cross_embed)
+            
+            else:
+                with torch.no_grad():
+                    embed = model.compute_audio_embeds(audio)
+                # remove batch dimension:
+                embed = embed.squeeze(dim=0)
+
+                assert tuple(embed.size()) == (model.output_embed_dim, ), "Error with shape of embed."
+                audio_embeds.append(embed)
+            
+            audio_tags.append(tag)
+    
+    if model.multi_task:
+        return audio_intra_embeds, audio_cross_embeds, audio_tags
+    else:
+        return audio_embeds, audio_tags
+
---
+++ tests/eval_tests.py
@@ -0,0 +1,250 @@
+"""Script for testing evaluation functions."""
+
+
+import argparse
+import torch
+import clip
+
+from climur.utils.eval import get_image_embeddings, get_audio_embeddings
+from climur.dataloaders.imac_images import IMACImages
+from climur.dataloaders.audioset import AudioSetMood
+from climur.models.image_backbones import CLIPModel
+from climur.models.audio_model_components import ShortChunkCNN_Res, HarmonicCNN
+from climur.models.audio_backbones import ShortChunkCNNEmbeddings, HarmonicCNNEmbeddings, SHORTCHUNK_INPUT_LENGTH, HARMONIC_CNN_INPUT_LENGTH
+from climur.trainers.image2music import Image2Music
+from climur.utils.misc import load_configs
+
+
+# default manual mapping from image dataset emotion tags to audio dataset emotion tags:
+IMAGE2AUDIO_TAG_MAP = {
+    "excitement": "exciting",
+    "contentment": "happy",
+    "amusement": "funny",
+    "anger": "angry",
+    "fear": "scary",
+    "sadness": "sad"
+}
+
+# default config file:
+CONFIG_FILE = "configs/config_retrieval_eval.yaml"
+# script options:
+verbose = True
+
+
+if __name__ == "__main__":
+    print("\n")
+
+    # parse command-line arguments:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", nargs="?", const=CONFIG_FILE, default=CONFIG_FILE)
+    args = parser.parse_args()
+
+    # set PyTorch warnings:
+    # torch.set_warn_always(False)
+
+
+    # -------
+    # CONFIGS
+    # -------
+
+    # load configs:
+    configs = load_configs(args.config_file)
+    # unpack configs:
+    dataset_configs = configs["dataset"]
+    image_backbone_configs = configs["image_backbone"]
+    audio_backbone_configs = configs["audio_backbone"]
+    full_model_configs = configs["full_model"]
+    eval_configs = configs["eval"]
+    
+    # get device:
+    gpu_id = eval_configs["gpu"]
+    device = torch.device(f"cuda:{gpu_id}") if torch.cuda.is_available() else torch.device("cpu")
+
+
+    # ---------------
+    # BACKBONE MODELS
+    # ---------------
+
+    if verbose:
+        print("\nSetting up backbone models...")
+    
+    # load CLIP model:
+    orig_clip_model, image_preprocess_transform = clip.load("ViT-B/32", device=device)
+    # create CLIP wrapper model:
+    image_backbone = CLIPModel(orig_clip_model)
+    image_backbone.to(device)
+
+    # set up audio backbone model:
+    if audio_backbone_configs["model_name"] == "ShortChunk":
+        # set audio input length:
+        audio_clip_length = SHORTCHUNK_INPUT_LENGTH
+        # create full Short-Chunk CNN ResNet model:
+        full_audio_backbone = ShortChunkCNN_Res()
+        # create wrapper model:
+        sample_audio_input = torch.rand((2, audio_clip_length))
+        audio_backbone = ShortChunkCNNEmbeddings(
+            full_audio_backbone,
+            sample_input=sample_audio_input,
+            last_layer=audio_backbone_configs["last_layer_embed"],
+            pool_type=audio_backbone_configs["pool_type"],
+        )
+    
+    elif audio_backbone_configs["model_name"] == "HarmonicCNN":
+        # set audio input length:
+        audio_clip_length = HARMONIC_CNN_INPUT_LENGTH
+        # create full Harmonic CNN model:
+        full_audio_backbone = HarmonicCNN()
+        # create wrapper model:
+        sample_audio_input = torch.rand((2, audio_clip_length))
+        audio_backbone = HarmonicCNNEmbeddings(
+            full_audio_backbone,
+            sample_input=sample_audio_input,
+            last_layer=audio_backbone_configs["last_layer_embed"],
+            pool_type=audio_backbone_configs["pool_type"],
+        )
+    
+    else:
+        raise ValueError("{} model not supported".format(audio_backbone_configs["model_name"]))
+    
+    audio_backbone.to(device)
+
+
+    # ------------
+    # DATA LOADERS
+    # ------------
+
+    if verbose:
+        print("\nSetting up datasets and data loaders...")
+    
+    # set up metadata file names:
+    if dataset_configs["subset"] == "val":
+        image_dataset_metadata_file = "metadata_val.csv"
+        audio_dataset_metadata_file = "new_split_metadata_files/metadata_val.csv"
+    elif dataset_configs["subset"] == "test":
+        image_dataset_metadata_file = "metadata_test.csv"
+        audio_dataset_metadata_file = "new_split_metadata_files/metadata_test.csv"
+    else:
+        raise ValueError("Invalid subset.")
+    
+    # create image dataset:
+    image_dataset = IMACImages(
+        root=dataset_configs["image_dataset_dir"],
+        metadata_file_name=image_dataset_metadata_file,
+        preprocess=image_preprocess_transform
+    )
+    # create audio dataset:
+    audio_dataset = AudioSetMood(     # TODO: Add eval=True when this is added to dataset class.
+        root=dataset_configs["audio_dataset_dir"],
+        metadata_file_name=audio_dataset_metadata_file,
+        clip_length_samples=audio_clip_length,
+        sample_rate=dataset_configs["sample_rate"],
+    )
+
+
+    # ----------
+    # FULL MODEL
+    # ----------
+
+    if verbose:
+        print("\nLoading pretrained full model...")
+    
+    full_model = Image2Music.load_from_checkpoint(
+        full_model_configs["checkpoint_path"],
+
+        image_backbone=image_backbone,
+        audio_backbone=audio_backbone,
+        output_embed_dim=full_model_configs["output_embed_dim"],
+        image_embed_dim=image_backbone_configs["embed_dim"],
+        audio_embed_dim=audio_backbone_configs["embed_dim"],
+
+        multi_task = full_model_configs["multi_task"],
+        base_proj_hidden_dim = full_model_configs["base_proj_hidden_dim"],
+        base_proj_dropout = full_model_configs["base_proj_dropout"],     # TODO: Probably can remove this.
+        base_proj_output_dim = full_model_configs["base_proj_output_dim"],
+        task_proj_dropout = full_model_configs["task_proj_dropout"],     # TODO: Probably can remove this.
+
+        normalize_image_embeds=full_model_configs["normalize_image_embeds"],
+        normalize_audio_embeds=full_model_configs["normalize_audio_embeds"],
+        freeze_image_backbone=full_model_configs["freeze_image_backbone"],     # TODO: Probably can just set to True.
+        freeze_audio_backbone=full_model_configs["freeze_audio_backbone"],     # TODO: Probably can just set to True.
+        device=device
+    )
+    full_model.to(device)
+    # set to eval mode:
+    full_model.eval()
+
+
+    # -----
+    # TESTS
+    # -----
+
+    # set image and audio dataset emotion tags:
+    image_dataset_tags = list(IMAGE2AUDIO_TAG_MAP.keys())
+    audio_dataset_tags = list(IMAGE2AUDIO_TAG_MAP.values())
+
+    # test get_image_embeddings() function:
+    if verbose:
+        print()
+        print("\n\nTesting get_image_embeddings() function...")
+    
+    if full_model_configs["multi_task"]:
+        image_intra_embeds, image_cross_embeds, image_tags = get_image_embeddings(
+            model=full_model,
+            image_dataset=image_dataset,
+            image_dataset_tags=image_dataset_tags,
+            device=device
+        )
+    else:
+        image_embeds, image_tags = get_image_embeddings(
+            model=full_model,
+            image_dataset=image_dataset,
+            image_dataset_tags=image_dataset_tags,
+            device=device
+        )
+    
+    # tests:
+    if full_model_configs["multi_task"]:
+        assert len(image_intra_embeds) == len(image_cross_embeds) and len(image_cross_embeds) == len(image_tags), "List lengths don't match."
+        assert tuple(image_intra_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of image_intra_embeds tensors."
+        assert tuple(image_cross_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of image_cross_embeds tensors."
+    else:
+        assert len(image_embeds) == len(image_tags), "List lengths don't match."
+        assert tuple(image_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of image_embeds tensors."
+    if verbose:
+        print("Extracted embeddings of {} images.".format(len(image_tags)))
+    
+
+    # test get_audio_embeddings() function:
+    if verbose:
+        print()
+        print("\n\nTesting get_audio_embeddings() function...")
+    
+    if full_model_configs["multi_task"]:
+        audio_intra_embeds, audio_cross_embeds, audio_tags = get_audio_embeddings(
+            model=full_model,
+            audio_dataset=audio_dataset,
+            audio_dataset_tags=audio_dataset_tags,
+            device=device
+        )
+    else:
+        audio_embeds, audio_tags = get_audio_embeddings(
+            model=full_model,
+            audio_dataset=audio_dataset,
+            audio_dataset_tags=audio_dataset_tags,
+            device=device
+        )
+    
+    # tests:
+    if full_model_configs["multi_task"]:
+        assert len(audio_intra_embeds) == len(audio_cross_embeds) and len(audio_cross_embeds) == len(audio_tags), "List lengths don't match."
+        assert tuple(audio_intra_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of audio_intra_embeds tensors."
+        assert tuple(audio_cross_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of audio_cross_embeds tensors."
+    else:
+        assert len(audio_embeds) == len(audio_tags), "List lengths don't match."
+        assert tuple(audio_embeds[0].size()) == (full_model.output_embed_dim, ), "Error with shape of audio_embeds tensors."
+    if verbose:
+        print("Extracted embeddings of {} audio clips.".format(len(audio_tags)))
+    
+
+    print("\n\n")
+
