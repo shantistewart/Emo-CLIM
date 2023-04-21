fad6af7c94f14a11cdc3da069682690abe32ff1e
klean2050 <k.avramidis@windowslive.com>
2023-04-20 17:36:53 -0700

    add clip length utils

--- climur/dataloaders/mtat.py
+++ climur/dataloaders/mtat.py
@@ -1,4 +1,5 @@
-import os, numpy as np, torchaudio, subprocess
+import os, numpy as np, subprocess
+import torch, torchaudio
 from typing import Tuple, Optional
 from torch.utils.data import Dataset
 
@@ -85,6 +86,7 @@ class MTAT(Dataset):
         subset: Optional[str] = None,
         split: Optional[str] = "pons2017",
         sr: int = 22050,
+        duration: int = 30,
     ) -> None:
 
         super(MTAT, self).__init__()
@@ -94,6 +96,7 @@ class MTAT(Dataset):
         self.download = download
         self.subset = subset
         self.split = split
+        self.duration = duration
 
         assert subset is None or subset in ["train", "valid", "test"], (
             "When `subset` not None, it must take a value from " + "{'train', 'valid', 'test'}."
@@ -166,14 +169,52 @@ class MTAT(Dataset):
             )
             p.wait()
 
+    def segment_audio_sample(self):
+        audio_len = audio.shape[0]
+        segment_len = int(self.duration * self.sr)
+
+        if audio_len < segment_len:
+            audio = torch.cat([audio, torch.zeros(segment_len - audio_len)])
+
+        if self.subset == "train":
+            # randomly crop to target clip length:
+            start_idx = np.random.randint(low=0, high=audio_len - self.duration + 1)
+            end_idx = start_idx + self.duration
+            audio = audio[start_idx:end_idx]
+            assert audio.size(dim=0) == self.duration, "Error with cropping audio clip."
+
+        else:
+            # split audio clip into chunks:
+            step = int(np.around((1 - self.overlap_ratio) * self.duration))
+            audio_chunks = audio.unfold(dimension=0, size=self.duration, step=step)
+            # sanity check shape:
+            assert (
+                len(tuple(audio_chunks.size())) == 2 and audio_chunks.size(dim=-1) == self.duration
+            ), "Error with shape of chunked audio clip."
+
+        return audio
+
     def __getitem__(self, n: int) -> Tuple[Tensor, Tensor]:
         clip_id, _ = self.fl[n].split("\t")
         label = self.binary[int(clip_id)]
 
         filepath = self.file_path(n).replace(".mp3", ".wav")
         audio, sr = torchaudio.load(filepath)
-        resample = torchaudio.transforms.Resample(sr, self.sr)
-        return resample(audio), FloatTensor(label)
+        if sr != self.sr:
+            resample = torchaudio.transforms.Resample(sr, self.sr)
+            audio = resample(audio)
+        print(audio.shape)
+        audio = self.segment_audio_sample(audio.squeeze(0))
+        return audio, FloatTensor(label)
 
     def __len__(self) -> int:
         return len(self.fl)
+
+
+if __name__ == "__main__":
+    dataset = MTAT(
+        root="/data/avramidi/VCMR/data/magnatagatune", download=False, subset="valid", sr=16000
+    )
+    print(len(dataset))
+    print(dataset[0][0].shape)
+    print(dataset[0][1].shape)
