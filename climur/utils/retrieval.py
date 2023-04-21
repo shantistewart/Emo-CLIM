f800d104c402787a7f481a4358e8af29847e24a0
klean2050 <k.avramidis@windowslive.com>
2023-04-20 17:22:38 -0700

    black formatting

--- climur/utils/retrieval.py
+++ climur/utils/retrieval.py
@@ -12,7 +12,15 @@ from typing import Dict, List, Any
 SUPPORTED_RETRIEVAL_METRICS = ["precision", "MRR"]
 
 
-def compute_retrieval_metrics(query_embeds: List, query_labels: List, item_embeds: List, item_labels: List, metric_names: List, k_vals: List, device: Any) -> Dict:
+def compute_retrieval_metrics(
+    query_embeds: List,
+    query_labels: List,
+    item_embeds: List,
+    item_labels: List,
+    metric_names: List,
+    k_vals: List,
+    device: Any,
+) -> Dict:
     """Computes various retrieval metrics.
 
     Args:
@@ -23,21 +31,32 @@ def compute_retrieval_metrics(query_embeds: List, query_labels: List, item_embed
         metric_names (list): List of retrieval metric names to compute.
         k_vals (list): List of k values for retrieval metrics.
         device (PyTorch device): PyTorch device.
-    
+
     Returns:
         mean_metrics (dict): Dictionary of retrieval metrics.
     """
 
     # sanity checks:
-    assert len(query_embeds) == len(query_labels), "Query embedding and label list lengths don't match."
-    assert len(item_embeds) == len(item_labels), "Item embedding and label list lengths don't match."
-    assert tuple(query_embeds[0].size()) == tuple(item_embeds[0].size()), "Query and item embedding dimensions don't match."
+    assert len(query_embeds) == len(
+        query_labels
+    ), "Query embedding and label list lengths don't match."
+    assert len(item_embeds) == len(
+        item_labels
+    ), "Item embedding and label list lengths don't match."
+    assert tuple(query_embeds[0].size()) == tuple(
+        item_embeds[0].size()
+    ), "Query and item embedding dimensions don't match."
     # validate metric names:
-    assert set(metric_names) <= set(SUPPORTED_RETRIEVAL_METRICS), "At least one metric in metric_names isn't supported."
+    assert set(metric_names) <= set(
+        SUPPORTED_RETRIEVAL_METRICS
+    ), "At least one metric in metric_names isn't supported."
 
     # convert item embeddings to tensor:
     item_embeds = torch.stack(item_embeds, dim=0)
-    assert tuple(item_embeds.size()) == (len(item_labels), query_embeds[0].size(dim=0)), "Error converting list to tensor."
+    assert tuple(item_embeds.size()) == (
+        len(item_labels),
+        query_embeds[0].size(dim=0),
+    ), "Error converting list to tensor."
 
     # initialize dictionary for storing all per-query retrieval metrics:
     all_metrics = {}
@@ -48,23 +67,33 @@ def compute_retrieval_metrics(query_embeds: List, query_labels: List, item_embed
                 all_metrics[name][f"k={k}"] = []
         elif name == "MRR":
             all_metrics[name][f"k=N/A"] = []
-    
+
     # compute retrieval metrics:
-    for idx in tqdm.tqdm(range(len(query_labels)), total=len(query_labels), desc="Computing retrieval metrics"):
+    for idx in tqdm.tqdm(
+        range(len(query_labels)), total=len(query_labels), desc="Computing retrieval metrics"
+    ):
         # compute cosine similarity scores between query embedding and all item embeddings:
         query_embed = query_embeds[idx].unsqueeze(dim=0)
-        cos_sim_scores = F.cosine_similarity(query_embed, item_embeds, dim=-1)     # (1, embed_dim) * (n_items, embed_dim) -> (n_items, )
+        cos_sim_scores = F.cosine_similarity(
+            query_embed, item_embeds, dim=-1
+        )  # (1, embed_dim) * (n_items, embed_dim) -> (n_items, )
 
         # map cosine similarity values from [-1.0, 1.0] -> [0.0, 1.0]:
         sim_scores = 0.5 * (cos_sim_scores + 1.0)
-        assert tuple(sim_scores.size()) == (len(item_labels), ), "Similarity scores has an unexpected shape."
-        assert torch.min(sim_scores).item() >= 0.0 and torch.max(sim_scores).item() <= 1.0, "Similarity values are not in range [0.0, 1.0]"
+        assert tuple(sim_scores.size()) == (
+            len(item_labels),
+        ), "Similarity scores has an unexpected shape."
+        assert (
+            torch.min(sim_scores).item() >= 0.0 and torch.max(sim_scores).item() <= 1.0
+        ), "Similarity values are not in range [0.0, 1.0]"
 
         # create retrieval ground-truth:
         retrieval_gt = np.equal(query_labels[idx], np.asarray(item_labels, dtype=object))
         retrieval_gt = torch.from_numpy(retrieval_gt)
         retrieval_gt = retrieval_gt.to(device)
-        assert tuple(retrieval_gt.size()) == tuple(sim_scores.size()), "Error creating retrieval ground-truth."
+        assert tuple(retrieval_gt.size()) == tuple(
+            sim_scores.size()
+        ), "Error creating retrieval ground-truth."
 
         # compute retrieval metrics:
         for name in metric_names:
@@ -75,13 +104,12 @@ def compute_retrieval_metrics(query_embeds: List, query_labels: List, item_embed
             elif name == "MRR":
                 reciprocal_rank = retrieval_reciprocal_rank(sim_scores, retrieval_gt).item()
                 all_metrics[name][f"k=N/A"].append(reciprocal_rank)
-    
+
     # compute mean of per-query retrieval metrics over all queries:
     mean_metrics = {}
     for name, metrics in all_metrics.items():
         mean_metrics[name] = {}
         for k_str in metrics.keys():
             mean_metrics[name][k_str] = np.mean(np.asarray(metrics[k_str]))
-    
-    return mean_metrics
 
+    return mean_metrics
